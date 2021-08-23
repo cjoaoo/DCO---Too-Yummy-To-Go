826b650069dd42210c7e72ef960182f0b96a4a0b
Catarina Fernandes <catarinafernn@gmail.com>
2021-08-23 18:33:00 +0100

    projeto DCO

---
+++ TooYummyToGo/src/pt/tooyummytogo/pagamento/MeioPagamentoOwner.java
@@ -0,0 +1,35 @@
+package pt.tooyummytogo.pagamento;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import pt.tooyummytogo.config.Configuration;
+
+
+public class MeioPagamentoOwner {
+
+	@SuppressWarnings("unchecked")
+	public static MeioPagamento getMeioPagamento(String numero, String validade, String ccv) {
+		
+		Configuration config = Configuration.getInstance();	
+		String className = config.getProperty("meioPagamentoAUsar");
+		Class<MeioPagamento> klass;
+		try {
+			klass = (Class<MeioPagamento>) Class.forName(className);
+			Constructor<MeioPagamento> cons;
+			cons = klass.getConstructor(String.class, String.class, String.class);
+			return cons.newInstance(numero, validade, ccv);
+		} catch (NullPointerException | ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException 
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// do nothing
+		} 
+			
+		// plano B caso o config falhe:
+		
+		//throw new ErroCartaoException("Erro no processamento do cartão");
+		return new MonsterCardAdapter(numero, validade, ccv);
+	}
+	
+	
+
+}
